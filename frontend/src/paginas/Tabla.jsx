7aac8d53fed1a9950360ee8eb67498d04334331e
ElizabethCT26 <mariaelizabethchuctun@gmail.com>
2023-10-25 08:27:46 -0500

    Front

--- frontend/src/paginas/Tabla.jsx
+++ frontend/src/paginas/Tabla.jsx
@@ -7,7 +7,7 @@ function Tabla() {
     <h2 className="w-full flex-col text-center font-krona text-[#159D95] text-4xl p-5">CUADRO DE MEDICAMENTOS</h2>
     <div className='bg-white rounded-sm p-2 w-[80%] mx-auto h-[90%] border-x-2 border-b-4 border-t flex items-center  justify-center flex-wrap py-20'>
       <main>
-         <table>
+      <table className='my-0.5'>
          <thead>
          <tr className='items-center justify-center '>
          <th className='bg-[#FF9688] w-40 h-28 border-r-2'>Primera columna</th> 
@@ -17,7 +17,8 @@ function Tabla() {
          <th className='bg-[#FF9688] w-40 h-28 border-r-2'>Quinta columna</th>
          </tr>
          </thead> 
-
+         </table>
+         <table className='my-0.5'>
          <thead>
          <tr className='items-center justify-center'>
          <th className='bg-[#FFDBAF] w-40 h-28 border-r-2'>Primera columna</th> 
@@ -27,7 +28,9 @@ function Tabla() {
          <th className='bg-[#FFF9CE] w-40 h-28 border-r-2'>Quinta columna</th>
          </tr>
          </thead> 
+         </table>
 
+         <table className='my-0.5'>
          <thead>
          <tr className='items-center justify-center'>
          <th className='bg-[#5DC1B9] w-40 h-28 border-r-2'>Primera columna</th> 
@@ -37,8 +40,10 @@ function Tabla() {
          <th className='bg-[#8BDFD8] w-40 h-28 border-r-2'>Quinta columna</th>
          </tr>
          </thead> 
+         </table>
 
-         <thead>
+         <table className='my-0.5'>
+         <thead >
          <tr className='items-center justify-center'>
          <th className='bg-[#A080A3] w-40 h-28 border-r-2'>Primera columna</th> 
          <th className='bg-[#C096C4] w-40 h-28 border-r-2'>Segunda columna</th>
@@ -47,7 +52,9 @@ function Tabla() {
          <th className='bg-[#C096C4] w-40 h-28 border-r-2'>Quinta columna</th>
          </tr>
          </thead> 
-
+         </table>
+      
+         <table className='my-0.5'>
          <thead>
          <tr className='items-center justify-center'>
          <th className='bg-[#7BDD74] w-40 h-28 border-r-2'>Primera columna</th> 
@@ -57,11 +64,7 @@ function Tabla() {
          <th className='bg-[#B9FFB3] w-40 h-28 border-r-2'>Quinta columna</th>
          </tr>
          </thead> 
-
-
          </table>
-       
-
    </main>
    </div>
    </div>
