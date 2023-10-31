5e5cfb71b7b3d1c45299b4af4cb57cf73b2a1393
ElizabethCT26 <mariaelizabethchuctun@gmail.com>
2023-10-31 16:48:40 -0500

    Front con sesion

--- frontend/src/paginas/Tabla.jsx
+++ frontend/src/paginas/Tabla.jsx
@@ -9,12 +9,16 @@ import axios from 'axios';
 
 function Tabla() {
   const [abierto, setAbierto] = useState(false);
-
+  const [isopen, setIsopen] = useState(false);
 
   return (
     <>
       <div>
         <div>
+          <div className='flex justify-end'>
+          <img className='w-14 h-14 m-[1%] absolute' src='https://cdn-icons-png.flaticon.com/128/3177/3177440.png' onClick={() => { setIsopen(true) }}></img>
+          
+          </div>
           <h2 className="w-full flex-col text-center font-krona text-[#159D95] text-4xl p-5">CUADRO DE MEDICAMENTOS</h2>
         </div>
 
@@ -84,8 +88,45 @@ function Tabla() {
         </div>
       </div>
 
+
+
+
+
+
+
+
+      {/*Inicio modal*/}
+      
+      { isopen &&(
+        <div className="absolute top-20 right-3 flex justify-end items-start">
+        <div className="bg-white p-5 rounded border border-gray-100  flex flex-col justify-start items-center h-2/2 w-2/2 ">
+        <button
+            className="font-bold text-sm hover:text-gray-400 text-black absolute top-0 right-0 p-2"
+            onClick={() => setIsopen(false)}>
+            X
+          </button>
+
+          <div className="w-3/4 flex flex-col justify-end items-center">
+            <button className="text-white px-2 border bg-teal-500 rounded-md w-32" >
+              Cambiar Perfil
+            </button>
+            <button className="text-white bg-red-600  m-3 px-2 border border-red-600 rounded-md w-32">
+              Cerrar Sesion
+            </button>
+          </div>
+        </div>
+        </div>
+      )
+      }
+     
+      {/*Final modal*/}
+
+
+
+
       <Agregar abierto={abierto} setAbierto={setAbierto}/>
     </>
+    
   )
 }
 
